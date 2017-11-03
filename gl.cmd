284baeac4e38e2897d01be0fec5707f7956e0fe3
Hywel Rees <hjr555@gmail.com>
2017-11-03 21:13:23 +0000

    git log alias

---
+++ gl.cmd
@@ -0,0 +1,1 @@
+@git log --format="%%C(Cyan bold)[%%Cred%%h%%Creset%%C(Cyan bold)]%%Creset %%C(Green bold)%%an%%Creset - %%C(White bold)%%s%%Creset (%%C(Magenta)%%ar%%Creset)" --graph --max-count=20
