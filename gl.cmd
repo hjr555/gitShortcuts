a42ecc354e1c1bbe9c546145f2c4ea5c161b5487
Hywel Rees <hjr555@gmail.com>
2017-11-03 21:54:35 +0000

    Add missing bracket

--- gl.cmd
+++ gl.cmd
@@ -1,1 +1,1 @@
-@git log --format="%%C(cyan bold)[%%C(red dim)%%h%%C(reset)%%C(cyan bold)]%%C(reset) %%C(green bold)%%an%%C(reset) - %%C(white bold)%%s%%C(reset) (%%C(magenta)%%ar%%C(reset)" --graph --max-count=20 --decorate=full
+@git log --format="%%C(cyan bold)[%%C(red dim)%%h%%C(reset)%%C(cyan bold)]%%C(reset) %%C(green bold)%%an%%C(reset) - %%C(white bold)%%s%%C(reset) (%%C(magenta)%%ar%%C(reset))" --graph --max-count=20 --decorate=full --all
